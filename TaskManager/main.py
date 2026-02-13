6d7ced76cf440b1285073ab9ceaf4a4da7d747fb
AoN_ToR <anton92@outlook.fr>
2026-02-13 12:02:31 +0100

    Add file.json

--- TaskManager/main.py
+++ TaskManager/main.py
@@ -4,36 +4,53 @@ import time
 print("Welcome to the task manager. Please follow the instructions.")
 time.sleep(3)
 
-file = "data.json"
+file = "file.json"
 
-def searchInFile(file, taskName):
-    with open(file, "r") as f:
-        if taskName in f:
-            return True
-        return False
-    
-def addToFile(file, taskName, taskDescription):
-    with open(file, "a") as f:
-        f.write(taskName + ":" + taskDescription)
-
-
-def addTask():
+def addTask(f_dict):
     taskName = input("Task title: ")
     taskDescription = input("Task description: ")
-    if searchInFile(file, taskName):
+    if taskName in f_dict.key():
         print("Already there")
         return
-    addToFile(file, taskName, taskDescription)
-    return
-def delTask():
-    taskName = input("Task title: ")
-    if searchInFile:
-        delFromFile(file, taskName)
-        return
-    print("No such task in file.")
-    return
-        
+    f_dict[taskName] = taskDescription
+    print("Task has been added.")
+    return f_dict
 
+def delTask(f_dict):
+    taskName = input("Task title: ")
+    if taskName in f_dict.key():
+        f_dict.pop(taskName)
+        print("Task has been removed.")
+        return
+    print("No such task is currently saved.")
+    return f_dict
+
+
+def loadFile(file):
+    with open(file, 'r') as f:
+        f_dict = json.loads(f)
+    return f_dict
+# at the end of the script, saves changes
+def saveChanges(file, f_dict):
+    with open(file, 'w') as f:
+        f.write(json.dump(f_dict))
+    print("File Saved")
+    return
+
+
+f_dict = loadFile("file.json")
 while True:
-    user_input = input("Choose what to do next: \n(1) Ajouter tâches\n(2) Supprimer tâches\n(3) Voir tâches\n(4) Quitter")
-    
+    user_input = input("Choose what to do next: \n(1) Add task\n(2) Delete task\n(3) See tasks\n(4) Quit")
+    if user_input == "1" | user_input == "(1)":
+        f_dict = addTask(f_dict)
+    elif user_input == "2" | user_input == "(2)":
+        f_dict = delTask(f_dict)
+    elif user_input == "3" | user_input == "(3)":
+        for k,v in f_dict:
+            print(f"{k} : {v}", k, v)
+    elif user_input == "4" | user_input == "(4)":
+        break
+    else:
+        print("Given user input was not in the options. Please try again.")
+
+saveChanges(f_dict)
