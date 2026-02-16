e57bf0e1a0bf18bdf2837fda063a8300e13da23e
AoN_ToR <anton92@outlook.fr>
2026-02-16 12:23:54 +0100

    Adding a marked a done feature

--- TaskManager/main.py
+++ TaskManager/main.py
@@ -2,9 +2,24 @@ import json
 import time
 import sys
 import os
+import platform
 
 print("Welcome to the task manager. Please follow the instructions.")
 time.sleep(3)
+
+def next():
+    input("\n\nPress Enter to keep going.")
+    try:
+        # Detect operating system
+        current_os = platform.system()
+        
+        if current_os == "Windows":
+            os.system('cls')  # Windows
+        else:
+            os.system('clear')  # macOS/Linux
+    except Exception as e:
+        print(f"Error clearing terminal: {e}")
+
 
 # Get the directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -16,7 +31,7 @@ def addTask(f_dict):
         print("Name already used")
         return
     taskDescription = input("Task description: ")
-    f_dict[taskName] = taskDescription
+    f_dict[taskName] = {"description": taskDescription, "done":False}
     print("Task has been added.")
     return
 
@@ -29,6 +44,26 @@ def delTask(f_dict):
     print("No such task is currently saved.")
     return
 
+def seeTask(f_dict):
+    for k, v in f_dict.items():
+        desc = v["description"]
+        done = v["done"]
+        print(f"{k}: {desc} "+("(Done)" if done else "(Not done)"))
+
+def markDone(f_dict):
+    taskName = input("Enter Task Name: ")
+    if taskName in f_dict.keys():    
+        f_dict[taskName]["done"] = True
+        print(f"{taskName} is now marked as done !")
+    else:
+        print("Task not registered")
+def markUndone(f_dic):
+    taskName = input("Enter Task Name: ")
+    if taskName in f_dict.keys():    
+        f_dict[taskName]["done"] = False
+        print(f"{taskName} is now marked as undone !")
+    else:
+        print("Task not registered")
 
 def loadFile(file):
     try:
@@ -52,27 +87,41 @@ def saveChanges(file, f_dict):
 
 f_dict = loadFile(file)
 while True:
-    user_input = input("\nChoose what to do next: \n(1) Add task\n(2) Delete task\n(3) See tasks\n(4) Quit\n")
-    if user_input == "1" or user_input == "(1)":
+    user_input = input("\nChoose what to do next: \n(1) Add task\n(2) Delete task\n(3) Mark task as done\n(4) Mark task as undone\n(5) See tasks\n(6) Quit\n")
+    user_input = user_input.strip("() ")
+    if user_input == "1":
         sys.stdout.write("\033[F")
+        print("\r   ")
         addTask(f_dict)
-        print("\r ")
-    elif user_input == "2" or user_input == "(2)":
+        next()
+    elif user_input == "2":
         sys.stdout.write("\033[F")
-        print("\r ")
+        print("\r   ")
         delTask(f_dict)
-    elif user_input == "3" or user_input == "(3)":
+        next()
+    elif user_input == "3":
         sys.stdout.write("\033[F")
-        print("\r ")
-        for k,v in f_dict.items():
-            print(f"{k} : {v}")
-    elif user_input == "4" or user_input == "(4)":
+        print("\r   ")
+        markDone(f_dict)
+        next()
+    elif user_input == "4":
         sys.stdout.write("\033[F")
-        print("\r ")
+        print("\r   ")
+        markUndone(f_dict)
+        next()
+    elif user_input == "5":
+        sys.stdout.write("\033[F")
+        print("\r   ")
+        seeTask(f_dict)
+        next()
+    elif user_input == "6":
+        sys.stdout.write("\033[F")
+        print("\r   ")
         break
     else:
-        sys.stdout.write("\033[2K\r")
-        sys.stdout.write("Given user input was not in the options. Please try again.")
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\rGiven user input was not in the options. Please try again.\n")
+        next()
         
 try:
     saveChanges(file, f_dict)
